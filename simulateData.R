f9d9d392ac5cddb45bbbd0a31b7f4ca296dec744
plawrence <plawrencenw@gmail.com>
2013-01-18 15:11:48 -0700

    Fleshed out the data generation and added support for binning and laying out experimental treatments

--- simulateData.R
+++ simulateData.R
@@ -3,7 +3,7 @@
 
 ##Load required packages
 library(geoR)
-library(spatstat)
+library(RandomFields)
 
 ##Set the seed
 set.seed(359)
@@ -11,20 +11,103 @@ set.seed(359)
 PrintModelList() ## the complete list of implemented models - choose one!
 
 ##Create an empty dataset
-model <- "stable"
+model <- "exponenti"
 mean <- 0
-variance <- 10
-nugget <- 0 #noise around the structure
-scale <- 10  #structure/patchiness
-alpha <- 2 ## see help("CovarianceFct") for additional
+variance <- 30
+scale <- .5  #structure/patchiness
+range <- 40
+
+#Set the number of field columns and rows
+fieldcol=40
+fieldrow=40
+
 ## parameters of the covariance functions
 step <- 1 ## nicer, but also time consuming if step <- 0.1
-x <- seq(0, 100, step)
-y <- seq(0, 100, step)
+x <- seq(0, fieldcol-1, step)
+y <- seq(0, fieldrow-1, step)
 
+#Additional parameters for the "stable" model
+alpha <- 2 ## see help("CovarianceFct") for additional
+nugget <- 0 #noise around the structure
+
+#Setup for the "stable model" - ignore
 f <- GaussRF(x=x, y=y, model=model,grid=T, param=c(mean, variance, nugget,
                                                    scale, alpha))   #
 
+#simulate the exponential model
+soilN <- GaussRF(x=x, y=y, model=model,grid=T, param=c(mean,variance,scale,range))
+
+#Make realistic values for soil N, simulate uniform application and yield response.  Sd quickly generates noise depending on
+#the underlying variation
+soilN = soilN*2+40
+appN = matrix(100,nrow=fieldrow,ncol=fieldcol)
+yield = .5*(soilN+appN)+matrix(rnorm(fieldcol*fieldrow,mean=0,sd=2),ncol=fieldcol,nrow=fieldrow)
+
+#Plot it
 par(mfcol=c(1,2))
-image(x, y, f)
+image(x, y, soilN)
+image(x, y, yield)
 contour(f) 
+
+#Divide the soil N into 3 bins that will be used for experimentation
+bin1 <- soilN[soilN<quantile(soilN,.33)]
+bin2 <- soilN[soilN<quantile(soilN,.66) & soilN>quantile(soilN,.33)]
+bin3 <- soilN[soilN>quantile(soilN,.66)]
+
+bins = matrix(0,ncol=fieldcol,nrow=fieldrow)
+quant.33 <- quantile(soilN,.33)
+quant.66 <- quantile(soilN,.66)
+for (i in 1:fieldrow){
+  for (j in 1:fieldcol){
+    if (soilN[i,j] < quant.33){
+      bins[i,j]=1
+    } else if (soilN[i,j]<quant.66 & soilN[i,j]>quant.33){
+      bins[i,j]=2
+    } else {
+      bins[i,j]=3
+    }
+  }
+}
+
+image(x,y,bins)
+
+#Find areas in each bin that are 3 cells in height, then randomly apply 4 treatments
+
+#function to search individual columns
+findTreats = function(colvec,bin,binsize){
+  binvec = match(colvec,bin)
+  matchvec = vector(length=length(binvec))
+  count = 0
+  for (i in 1:length(binvec)){
+    if (!is.na(binvec[i])){
+      count =count+1
+      matchvec[i] = count
+    } else {
+      count = 0
+      matchvec[i] = 0
+    }
+  }
+  locationvec = which(matchvec==binsize)
+  treatmentvec = rep(0,length(binvec))
+  treatmentvec[locationvec]=bin
+  binsize = binsize - 1
+  treatmentvec[locationvec-binsize] = bin
+  while (binsize >0){
+    treatmentvec[locationvec-binsize] = bin
+    binsize = binsize - 1
+  }
+  return (treatmentvec)    
+}
+
+applyExperiment = function(dataset,bin,binsize,exprmnt.treatments){
+  exptvec = matrix(0,ncol=fieldcol,nrow=fieldrow)
+  for (i in 1:fieldcol){
+    colvec = as.vector(dataset[,i])
+    treatvec = findTreats(colvec,bin,binsize)
+    exptvec[,i] = treatvec
+  }
+  return (exptvec)
+}
+
+image(x,y,bins)
+image(x,y,applyExperiment(bins,3,3,1))
